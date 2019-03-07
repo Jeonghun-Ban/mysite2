9a49894bf87f6acd7a01ee3b22c74b629970c0a4
jeonghun <bjh5230@gmail.com>
2019-03-07 21:40:20 +0900

    All ALLOWED_HOSTS

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '%i(etl_y&yqq!!eh87f$#*=@t9yq@a
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
